ddb31d839154364a562d0efb99365f38a716d840
gwpjp <john.pearson.02@gmail.com>
2017-11-09 09:26:01 -0500

    Updated skeleton code.

--- countVowels.js
+++ countVowels.js
@@ -3,11 +3,30 @@ const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
 // The input to countVowels will be a string.
 // It will log the string as well as the number of vowels to the console.
 function countVowels(inString) {
+    // First, return the user's input
+    console.log();
 
+    // This variable will count the number of vowels
+    let vowelCount = 0;
 
+    // Fill in the for loop with the appropriate conditions.
+    // Your for loop should go through each letter of inString.
+    for (/*Put conditions here*/) {
+        // Check if the current letter is a vowel.
+        // You will need to put the current variable into the Array.includes method.
+        if (vowels.includes(/*Put current letter here*/)) {
+            // The letter is a vowel so add 1 to the count of vowels
+            vowelCount += 1;
+        }
+    }
+
+    // Log the final result to the console.
+    console.log(`It has ${vowelCount} vowels.`);
 }
 
 
+countVowels('Hello');
+countVowels('My name is Sarah.');
 
 
 
